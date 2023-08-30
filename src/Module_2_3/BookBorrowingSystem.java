72f9c78d9cd324076b56d7304c7a54679e05e922
Markus <markus.salin3@metropolia.fi>
2023-08-30 14:58:51 +0300

    BookBorrowingSystem WIP

--- src/Module_2_3/BookBorrowingSystem.java
+++ src/Module_2_3/BookBorrowingSystem.java
@@ -1,8 +1,36 @@
 package Module_2_3;
 
 
+import java.util.ArrayList;
+
+
+class LibraryBorrow extends Library {
+    private final ArrayList<Book> books = new ArrayList<>();
+    private final ArrayList<Book> borrowedBooks = new ArrayList<>();
+
+    void borrowBooks(String title) {
+        for (Book book : books) {
+            // WIP
+        }
+    }
+}
+
+
 public class BookBorrowingSystem {
     public static void main(String[] args) {
+        LibraryBorrow libraryBorrow = new LibraryBorrow();
 
+        Book lordOfTheRings = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954);
+        Book aGameOfThrones = new Book("A Game of Thrones", "George R.R. Martin", 1996);
+
+        libraryBorrow.addBook(lordOfTheRings);
+        libraryBorrow.addBook(aGameOfThrones);
+
+        libraryBorrow.displayBooks();
+
+        libraryBorrow.findBooksByAuthor("J.R.R. Tolkien");
+        libraryBorrow.borrowBooks("A Game of Thrones");
+
+        libraryBorrow.displayBooks();
     }
 }

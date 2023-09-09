c9a2a1e6a0ee8b344c1331cbb19309091f26db34
Make <juhani.markus@gmail.com>
2023-09-09 19:43:06 +0300

    FibonacciSequence done NetworkCSVReader done update .gitignore

--- src/Module_3_4/FibonacciSequence.java
+++ src/Module_3_4/FibonacciSequence.java
@@ -1,4 +1,33 @@
 package Module_3_4;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class FibonacciSequence {
+    public static void main(String[] args) {
+        int n = 100;
+        String csvFileName = "fibonacci.csv";
+
+        long[] fibonacciNumbers = new long[n];
+        fibonacciNumbers[0] = 0;
+        fibonacciNumbers[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+        }
+
+        try (FileWriter fileWriter = new FileWriter(csvFileName);
+             PrintWriter printWriter = new PrintWriter(fileWriter)) {
+
+            for (long number : fibonacciNumbers) {
+                printWriter.println(number);
+            }
+
+            System.out.printf("Wrote Fibonacci Sequence to %s -file", csvFileName);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/Module_3_4/NetworkCSVReader.java
+++ src/Module_3_4/NetworkCSVReader.java
@@ -1,6 +1,60 @@
 package Module_3_4;
 
-// Unable to finish this exercise, since the website address is broken.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NetworkCSVReader {
+    public static void main(String[] args) {
+
+        URL url = null;
+        double sum = 0.0;
+        int count = 0;
+        List<String> lines = new ArrayList<>();
+
+        try {
+            url = new URL("https://users.metropolia.fi/~olliv/olio/korvike.csv");
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        try (InputStream inputStream = url.openStream();
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.startsWith("1.1.2023")) {
+                    lines.add(line);
+                }
+            }
+
+            bufferedReader.close();
+            inputStreamReader.close();
+            inputStream.close();
+
+            for (String s : lines) {
+                String[] parts = s.split(",");
+                if (parts.length >= 4) {
+                    double temperature = Double.parseDouble(parts[3].trim());
+                    sum += temperature;
+                    count++;
+                }
+            }
+
+            double average = sum / count;
+            System.out.printf("Average temperature on 1.1.2023 was %.2f°C", average);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(2);
+        }
+    }
 }

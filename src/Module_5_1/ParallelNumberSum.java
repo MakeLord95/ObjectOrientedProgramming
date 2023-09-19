071234a29b1e27ba8833204c068ae1ca3d62c324
Make <juhani.markus@gmail.com>
2023-09-19 17:16:32 +0300

    Module 5.1: ParallelNumberSum done

--- src/Module_5_1/ParallelNumberSum.java
+++ src/Module_5_1/ParallelNumberSum.java
@@ -1,6 +1,57 @@
 package Module_5_1;
 
-public class ParallelNumberSum {
+import java.util.Arrays;
+
+public class ParallelNumberSum extends Thread {
+    private int sum;
+    private final int[] numbers;
+
+    public ParallelNumberSum(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public void run() {
+        for (int number : numbers) {
+            sum += number;
+        }
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
     public static void main(String[] args) {
+        int[] numbers = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            numbers[i] = i + 1;
+        }
+
+        int numberOfThreads = 10;
+        int batchSize = numbers.length / numberOfThreads;
+
+        ParallelNumberSum[] parallelNumberCalculator = new ParallelNumberSum[numberOfThreads];
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            int startIndex = i * batchSize;
+            int endIndex = (i + 1) * batchSize;
+
+            int[] batchNumbers = Arrays.copyOfRange(numbers, startIndex, endIndex);
+
+            parallelNumberCalculator[i] = new ParallelNumberSum(batchNumbers);
+            parallelNumberCalculator[i].start();
+        }
+
+        int totalSum = 0;
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
+                parallelNumberCalculator[i].join();
+                totalSum += parallelNumberCalculator[i].getSum();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.printf("The total sum of all numbers is %d\n", totalSum);
     }
 }
